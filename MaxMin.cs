47ef2ce964fd0682f11dda3bd8c22c6ecdc184cb
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-24 17:33:11 +0530

    Add : Checked TestCases For MinMax

---
+++ MaxMin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMIn
+{
+    public class MaxMin
+    {
+        public static int MaximumIntNum(int fNumber, int sNumber, int thirdNumber)
+        {
+            if ((fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) > 0) ||
+                (fNumber.CompareTo(sNumber) >= 0 && fNumber.CompareTo(thirdNumber) > 0) ||
+                (fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) >= 0))
+            {
+                return fNumber;
+            }
+
+            if ((sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) > 0) ||
+               (sNumber.CompareTo(fNumber) >= 0 && sNumber.CompareTo(thirdNumber) > 0) ||
+               (sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) >= 0))
+            {
+                return sNumber;
+            }
+
+
+            if ((thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) > 0) ||
+               (thirdNumber.CompareTo(fNumber) >= 0 && thirdNumber.CompareTo(sNumber) > 0) ||
+               (thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) >= 0))
+            {
+                return thirdNumber;
+            }
+            return fNumber;
+        }
+    }
+}
