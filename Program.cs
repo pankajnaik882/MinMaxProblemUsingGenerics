c63910fd8a3d700a1fe5176059bd578a1274349d
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-24 17:37:39 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -4,8 +4,23 @@
     {
         public static void Main(string[] args)
         {
-            double max = MaxMin.MaximumIntNum(1.2,2.5,3.8); 
-            Console.WriteLine("{0} Greatest than Two ",max);
+            //double max = MaxMin.MaximumIntNum(1.2,2.5,3.8); 
+            //Console.WriteLine("{0} Greatest than Two ",max);
+
+            //int Max = MaxMin.MaximumIntNum(12, 25, 38);
+            //Console.WriteLine("{0} Greatest than Two ", Max);
+
+            string str = "Amit";
+            string str1 = "Sagar";
+            string str2 = "Krishna";
+
+            string Max = MaxMin.MaximumIntNum(str,str1,str2);
+            Console.WriteLine(Max);
+
+            //Console.WriteLine("String Length of First : " + str.Length);
+            //Console.WriteLine("String Length of Second : " + str1.Length);
+            //Console.WriteLine("String Length of Third: " + str2.Length);
+
         }
     }
 }
