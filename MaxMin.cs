d248eddbe3080f9646824270c3dab9e18de0fd13
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-24 17:49:15 +0530

    Add : Added Options & Checked String Values For MinMax

--- MaxMin.cs
+++ MaxMin.cs
@@ -6,66 +6,38 @@ using System.Threading.Tasks;
 
 namespace MaxMIn
 {
-    public class MaxMin<T> where T : IComparable
+    public class Maximum<T> where T : IComparable
     {
-        public T fNumber,sNumber,thirdNumber;
-        
-        public MaxMin(T fNumber,T sNumber, T thirdNumber) 
+        public T[] value;
+
+        public Maximum(T[] value)
         {
-            this.fNumber = fNumber;
-            this.sNumber = sNumber;
-            this.thirdNumber = thirdNumber;
-        }
-        public static T Maximum(T fNumber, T sNumber, T thirdNumber)
-        {
-            if ((fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) > 0) ||
-                (fNumber.CompareTo(sNumber) >= 0 && fNumber.CompareTo(thirdNumber) > 0) ||
-                (fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) >= 0))
-            {
-                return fNumber;
-            }
-
-            if ((sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) > 0) ||
-               (sNumber.CompareTo(fNumber) >= 0 && sNumber.CompareTo(thirdNumber) > 0) ||
-               (sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) >= 0))
-            {
-                return sNumber;
-            }
-
-
-            if ((thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) > 0) ||
-               (thirdNumber.CompareTo(fNumber) >= 0 && thirdNumber.CompareTo(sNumber) > 0) ||
-               (thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) >= 0))
-            {
-                return thirdNumber;
-            }
-           return fNumber;
+            this.value = value;
         }
 
-        /*   public static string MaximumIntNum(string first, string second, string third)
-           {
-               if(first.Length > second.Length && first.Length > third.Length)
-               {
-                   return "First is Greatest";
-               }
-               else if (second.Length > second.Length && second.Length > third.Length)
-               {
-                   return "Second is Greatest";
-               }
-               else if (third.Length > first.Length && third.Length > second.Length)
-               {
-                   return "Third is Greatest";
-               }
-               else
-               {
-                   return "Every string is same";
-               }
-           } */
-
-        public T maxvalue()
+        public T[] Sort(T[] values)
         {
-            T max = MaxMin<T>.Maximum(this.fNumber, this.sNumber, this.thirdNumber);
+            Array.Sort(values);
+            return values;
+        }
+
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
             return max;
+        }
+
+
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
         }
     }
 }
