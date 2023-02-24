a1aecabdad0a75aa84178c9d3c740599f28bfe32
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-24 17:29:32 +0530

    Add : Added Program.cs File

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MaxMIn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int max = MaxMin.MaximumIntNum(30, 20, 10); 
+            Console.WriteLine("{0} Greatest than Two ",max);
+        }
+    }
+}
